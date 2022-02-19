8fe05bf1006f93c2d98901b13eb4bf6d4688ae53
AgilDias <agildias.ad@gmail.com>
2022-02-19 10:47:50 +0700

    Pembagian

--- src/test/java/org/example/PembagianTest.java
+++ src/test/java/org/example/PembagianTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,6 +14,19 @@ public class PembagianTest {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://greyli.github.io/calculator/");
-    }
 
+        driver.findElement(By.xpath("//button[.='9']")).click();
+        driver.findElement(By.xpath("//button[.='9']")).click();
+        driver.findElement(By.xpath("//button[.='/']")).click();
+        driver.findElement(By.xpath("//button[.='1']")).click();
+        driver.findElement(By.xpath("//button[.='1']")).click();
+        driver.findElement(By.xpath("//button[@id='resultButton']")).click();
+        driver.findElement(By.xpath("//div[@id='output']")).getText();
+
+        String output = driver.findElement(By.xpath("//div[@id='output']")).getText();
+
+        Assert.assertEquals("9", output);
+
+        driver.quit();
+    }
 }
